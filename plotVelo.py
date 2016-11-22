1e0137d5f2b54cb771a960dcc955bb6ba75dc25c
diz_vara <avarfolomeev@navmii.com>
2016-11-22 15:31:43 +0300

    filter points in all dimensions
    show gray photos if there are no color ones
    plot first subplot in all cases

--- plotVelo.py
+++ plotVelo.py
@@ -12,37 +12,59 @@ import matplotlib.pyplot as plt
 def plot_velo(pnt,step=100, thr = 1.5, do3d = False):
     #f2 = plt.figure()
     #ax2 = f2.add_subplot(111, projection='3d')
+    fwlim = 60
+    backlim = -10
+    leftlim = -12
+    rightlim = 12
+    
     
     stp = step or 100
     if (do3d):
         ax1_3d.cla()
-        ax1_3d.axis([-15,10,-20,20])
-    else:
-        ax1.cla()
-        ax1.axis([-15,10,-20,20])
-    
+        ax1_3d.axis([leftlim,rightlim,backlim,fwlim])
 
-    high = dataset.velo[pnt][:,2] > -thr
-    numPoints = dataset.velo[pnt].shape[0]
+    ax1.cla()
+    ax1.axis([leftlim,rightlim,backlim,fwlim])
+    
+    v = dataset.velo[pnt][::-1]
+
+    #invertered x-axis!!
+    left = v[:,1] < -leftlim
+    right = v[:,1] > -rightlim
+    back = v[:,0] > backlim
+    fw = v[:,0] < fwlim
+    
+    
+    high = v[:,2] > -thr
+    hup = v[:,2] < 1
+    high = high & left & right & back & fw & hup
+    numPoints = v.shape[0]
     idx = np.arange(0,numPoints)[high]
 
     r = range(0, idx.size, stp)
     h = idx[r]
+
+    ax1.scatter(v[h, 1]*(-1),
+                v[h, 0],
+                #dataset.velo[pnt][h, 2],
+                c=v[h, 2]*50,
+                cmap='jet')
     
+
     if (do3d):
-        ax1_3d.scatter(dataset.velo[pnt][h, 1]*(-1),
-                    dataset.velo[pnt][h, 0],
-                    dataset.velo[pnt][h, 2],
-                    c=dataset.velo[pnt][h, 2]*50,
+        ax1_3d.scatter(v[h, 1]*(-1),
+                    v[h, 0],
+                    v[h, 2],
+                    c=v[h, 2]*50,
                     cmap='jet')
     else:
-        ax1.scatter(dataset.velo[pnt][h, 1]*(-1),
-                    dataset.velo[pnt][h, 0],
-                    #dataset.velo[pnt][h, 2],
-                    c=dataset.velo[pnt][h, 2]*50,
-                    cmap='jet')
+        ax4.imshow(dataset.gray[pnt].left,cmap='gray')
     
-    ax2.imshow(dataset.rgb[pnt].left)
-    ax3.imshow(dataset.rgb[pnt].right)
-    ax4.imshow(dataset.gray[pnt].left, cmap='gray')
+    if (hasattr(dataset,'rgb')):
+        ax2.imshow(dataset.rgb[pnt].left)
+        ax3.imshow(dataset.rgb[pnt].right)
+    elif (hasattr(dataset,'gray')):
+        ax2.imshow(dataset.gray[pnt].left,cmap='gray')
+        ax3.imshow(dataset.gray[pnt].right,cmap='gray')
+        
     plt.show()
