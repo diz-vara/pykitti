04bd87c26f7351a473c96184d256d2a3e42772af
diz_vara <avarfolomeev@navmii.com>
2018-04-24 15:47:49 +0300

    common imports in setFigure.py

--- setFigure.py
+++ setFigure.py
@@ -4,6 +4,9 @@ Created on Wed Oct 26 17:16:34 2016
 
 @author: avarfolomeev
 """
+import matplotlib.pyplot as plt
+import numpy as np
+from mpl_toolkits.mplot3d import Axes3D
 
 f2 = plt.figure()
 ax1 = f2.add_subplot(121)
