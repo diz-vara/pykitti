0e379544e3dee3bb54902236a1acdae9f3f7cdd4
diz_vara <avarfolomeev@navmii.com>
2018-05-18 16:07:55 +0300

    London variant of filter_cloud (without distance filtering)

--- pykitti/filter_cloud.py
+++ pykitti/filter_cloud.py
@@ -13,23 +13,28 @@ def filter_cloud(cloud):
     return cloud[filt,:], idx[filt]
         
 
-def filter_pts(pts):
+def filter_pts(pts, img_shape):
+    if (len(pts.shape) == 3):
+        pts = pts[:,0,:];
     pts=pts.round()
-    pts_idx= (pts[:,0] >= 0) & (pts[:,0]< 1920) & (pts[:,1] >= 0) & (pts[:,1] < 1200)
+    pts_idx= ( (pts[:,0] >= 0) & 
+               (pts[:,0]< img_shape[1]) & 
+               (pts[:,1] >= 0) & 
+               (pts[:,1] < img_shape[0]) )
     return pts_idx
     
     
 def get_colored_cloud(cloud, image, calibration):    
     global rot, t, mtx, dist
     
-    f_cloud,idx = filter_cloud(cloud)
+    #f_cloud,idx = filter_cloud(cloud)
     pts, jac = cv2.projectPoints(f_cloud, 
                                  calibration['rot'], 
                                  calibration['t'], 
                                  calibration['mtx'], 
                                  calibration['dist'])
     pts = pts[:,0,:]
-    pts_idx = filter_pts(pts)
+    pts_idx = filter_pts(pts,image.shape)
     ff_cloud = f_cloud[pts_idx,:]
     f_pts = np.round(pts[pts_idx]).astype(int)
     colors = image[f_pts[:,1],f_pts[:,0]]
@@ -37,5 +42,21 @@ def get_colored_cloud(cloud, image, calibration):
     return ff_cloud, colors, idx
     
 
+def l_get_colored_cloud(cloud, image, calibration):    
+    global rot, t, mtx, dist
+    
+    #f_cloud,idx = filter_cloud(cloud)
+    pts, jac = cv2.projectPoints(cloud, 
+                                 calibration['rot'], 
+                                 calibration['t'], 
+                                 calibration['mtx'], 
+                                 calibration['dist'])
+    pts = pts[:,0,:]
+    pts_idx = filter_pts(pts,image.shape)
+    ff_cloud = cloud[pts_idx,:]
+    f_pts = np.round(pts[pts_idx]).astype(int)
+    colors = image[f_pts[:,1],f_pts[:,0]]
+    idx = np.arange(len(cloud))[pts_idx]
+    return ff_cloud, colors, idx
 
 
