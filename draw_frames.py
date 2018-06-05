1f7a5b6518306339a32537e2ab9d4e185a3189fc
diz_vara <avarfolomeev@navmii.com>
2018-06-05 11:16:53 +0300

    translate points from file to file (without array in memory)

--- draw_frames.py
+++ draw_frames.py
@@ -9,15 +9,15 @@ cmap = ['blue','dodgerblue','cyan', 'lightgreen', 'green', 'yellow',
         'orange', 'red', 'magenta', 'brown',  'darkgray'];
         
         
-draw3d = False
-start = 420
+draw3d = True
+start = 1
 
 a2.cla()
 
-ff = np.arange(start,start+22,1)
+ff = np.arange(start,start+11,1)
 
 
-filter_h = (ptR1_11_12[:,2] > -1.4) & (ptR1_11_12[:,2] < 1.1)
+filter_h = (ptR1_11a[:,2] > -1.4) & (ptR1_11a[:,2] < 10.1)
 
 if (draw3d):
     a3.cla()
@@ -25,26 +25,26 @@ if (draw3d):
 
     
 for frame in ff:
-    filter_frame = (fR1_11_12==frame)
-    a2.scatter(ptR1_11_12[filter_frame & filter_h,0], 
-               ptR1_11_12[filter_frame & filter_h,1],
+    filter_frame = (fR1_11a==frame)
+    a2.scatter(ptR1_11a[filter_frame & filter_h,0], 
+               ptR1_11a[filter_frame & filter_h,1],
                marker='.',
                edgecolors='face',
                color=cmap[(frame-start)%11],s=1)
-    a2.scatter(nR1_11_12[filter_frame,0], 
-               nR1_11_12[filter_frame,1],marker='x',
+    a2.scatter(nR1_11a[filter_frame,0], 
+               nR1_11a[filter_frame,1],marker='x',
                edgecolors='face',
                color=cmap[(frame-start)%11],s=16)
 
     if (draw3d):
-        a3.scatter(0-ptR1_12[filter_frame,0], 
-                   ptR1_12[filter_frame,1],
-                   ptR1_12[filter_frame,2],
+        a3.scatter(0-ptR1_11a[filter_frame,0], 
+                   ptR1_11a[filter_frame,1],
+                   ptR1_11a[filter_frame,2],
                    marker='.',edgecolors='face',color=cmap[frame%11],s=1)
     
-        a3.scatter(0-nR1_12[filter_frame,0], 
-                   nR1_12[filter_frame,1],
-                   nR1_12[filter_frame,2],
+        a3.scatter(0-nR1_11a[filter_frame,0], 
+                   nR1_11a[filter_frame,1],
+                   nR1_11a[filter_frame,2],
                    marker='.',edgecolors='face',color=cmap[frame%11],s=3)
     
  #%%
--- translate_points.py
+++ translate_points.py
@@ -6,8 +6,16 @@ Created on Thu May 10 12:03:02 2018
 """
 import numpy as np
 
-def translate_points(points, imu_to_velo_rot, IMUdata,base=None, base_frame=0):
-    
+
+def translate_points(points_file, imu_to_velo_rot, 
+                     IMUdata,
+                     base=None, 
+                     base_frame=0):
+    points_struct_format = "dddiiiB";    
+    format_4f = 'fffBBBB'
+
+    point_color = np.zeros(4,np.uint8);    
+
     out = []
     IMU_idx = -1
     IMU_ts = extract_ts(IMUdata)
@@ -25,93 +33,118 @@ def translate_points(points, imu_to_velo_rot, IMUdata,base=None, base_frame=0):
     enus = []
     frames = []
     qs = []
+    point_size = struct.calcsize(points_struct_format)
     
-    pnt = points[0];
-    base_velo_time = copy.copy(pnt['ts']) + time_ref_corr;
-
-    print(base_velo_time)
-
-    while (IMU_idx < num_ts-3 and base_velo_time >= Ref_ts[IMU_idx+1] ):
-        IMU_idx += 1;
-    #this is the end    
-    if (IMU_idx >= num_ts) :
-        return;
-
-    print(IMU_idx)
-    ## - it's for LONDON!!! IMU_idx -= 2;
-    if (IMU_idx < 0):
-        IMU_idx = 0;
-        
-    base_time_ref = Ref_ts[IMU_idx];    
-
-
-    cnt = 0;
-        
+    points_num = os.path.getsize(points_file)/point_size;
     
-    nPoints = len (points)
-    step = nPoints//1000;
-    for pnt in points:
+    path = os.path.split(points_file)[0];
+    #frame_path = os.path.join(path,'frames');
+    #if os.path.exists(frame_path) is False:
+    #    os.makedirs(frame_path)
 
-        ts = pnt['ts'] + time_ref_corr;
+    ext = '.bin'
 
-        #time_ref = ts - base_velo_time + base_time_ref;
-        
-        while (IMU_idx < num_ts-1 and ts > Ref_ts[IMU_idx+1] ):
+    name = 'points_out' + ext;    
+    out_file = os.path.join(path,name);
+    
+    with open(points_file,'rb') as p_file, \
+         open(out_file, 'wb') as o_file:
+        buf = p_file.read(point_size);
+            
+        pnt = unpack(buf);
+
+        base_velo_time = copy.copy(pnt['ts']) + time_ref_corr;
+    
+        print(base_velo_time)
+    
+        while (IMU_idx < num_ts-3 and base_velo_time >= Ref_ts[IMU_idx+1] ):
             IMU_idx += 1;
         #this is the end    
-        if (IMU_idx >= num_ts - 1 ) :
-            break;
-
-            
-            
-        dt = (ts - Ref_ts[IMU_idx]).double();
-        time_step = (Ref_ts[IMU_idx+1] - Ref_ts[IMU_idx]).double();
-        
-        d_pos = (enu[IMU_idx+1]-enu[IMU_idx])/time_step*dt;   
-        q_int = q[IMU_idx].slerp(q[IMU_idx],q[IMU_idx+1], dt/time_step);                   
-        
-        pt = np.array([pnt['X'], pnt['Y'], pnt['Z']])
-        
-        
-        #if (pt[0] > 0.5) and (pt[0] < 120) and (pt[1] > -25) and (pt[1] < 35) and (pt[2] > -2.8) and (pt[2] < 100.5):
-        if (pt[0] > 0.5) and (pt[0] < 100) and (pt[1] > -30) and (pt[1] < 30) :
-            
-            pt = np.array(pt * imu_to_velo_rot)[0]
-            new_point = np.dot(pt,q_int.rotation_matrix) + enu[IMU_idx] + d_pos;
-                               
+        if (IMU_idx >= num_ts) :
+            return;
     
-            out.append(new_point)
-            #out.append(ned[IMU_idx] + d_pos)
-            frames.append(pnt['frame']+base_frame);
-            #diffs.append(pnt['frame']);
-            enus.append(enu[IMU_idx]+d_pos);
-            qs.append(q_int);            
-        cnt += 1;
-        if (cnt%step == 0):
-            print(cnt*100/nPoints,"%")
+        print(IMU_idx)
+        ## - it's for LONDON!!! IMU_idx -= 2;
+        if (IMU_idx < 0):
+            IMU_idx = 0;
+            
+        base_time_ref = Ref_ts[IMU_idx];    
+    
+    
+        cnt = 0;
+            
+        
+        step = points_num//1000;
+        while(1):
+    
+            ts = pnt['ts'] + time_ref_corr;
+    
+            #time_ref = ts - base_velo_time + base_time_ref;
+            
+            while (IMU_idx < num_ts-1 and ts > Ref_ts[IMU_idx+1] ):
+                IMU_idx += 1;
+            #this is the end    
+            if (IMU_idx >= num_ts - 1 ) :
+                break;
                 
-    print(IMU_idx)
+            dt = (ts - Ref_ts[IMU_idx]).double();
+            time_step = (Ref_ts[IMU_idx+1] - Ref_ts[IMU_idx]).double();
             
-    #manual zero-az point re-referencing
-    #ts = ROS_ts(1520430272,617093234);
-    #time_ref = ts - base_velo_time + base_time_ref;
-    #IMU_idx = -1;
-    #while (time_ref > Ref_ts[IMU_idx+1] and IMU_idx < num_ts):
-    #    IMU_idx += 1;
-
-    
-    #difference from one step, but I;ll use another ?????
-    #dt = (time_ref - Ref_ts[IMU_idx]).double();
-
+            d_pos = (enu[IMU_idx+1]-enu[IMU_idx])/time_step*dt;   
+            q_int = q[IMU_idx].slerp(q[IMU_idx],q[IMU_idx+1], dt/time_step);                   
+            
+            pt = np.array([pnt['X'], pnt['Y'], pnt['Z']])
+            
+            
+            #if (pt[0] > 0.5) and (pt[0] < 120) and (pt[1] > -25) and (pt[1] < 35) and (pt[2] > -2.8) and (pt[2] < 100.5):
+            if (pt[0] > 0.5) and (pt[0] < 50) and (pt[1] > -50) and (pt[1] < 50):
+                
+                pt = np.array(pt * imu_to_velo_rot)[0]
+                new_point = np.dot(pt,q_int.rotation_matrix) + enu[IMU_idx] + d_pos;
+                bf = struct.pack(format_4f,new_point[0], new_point[1], new_point[2], 
+                                 point_color[0],point_color[1],
+                                 point_color[2],point_color[3]);
+                o_file.write(bf);
+                                   
         
-    #time_step = (Ref_ts[IMU_idx+1] - Ref_ts[IMU_idx]).double();
-    #print (IMU_idx,dt,time_step)        
+                #out.append(new_point)
+                #out.append(ned[IMU_idx] + d_pos)
+                #frames.append(pnt['frame']+base_frame);
+                #diffs.append(pnt['frame']);
+                #enus.append(enu[IMU_idx]+d_pos);
+                #qs.append(q_int);
+            buf = p_file.read(point_size);
+            if (len(buf) < point_size):
+                break;
+            pnt = unpack(buf);
+            cnt += 1;
+            if (cnt%step == 0):
+                print(cnt*100/points_num,"%")
+            
+        p_file.close()
+        o_file.close()        
+        print(IMU_idx)
+                
+        #manual zero-az point re-referencing
+        #ts = ROS_ts(1520430272,617093234);
+        #time_ref = ts - base_velo_time + base_time_ref;
+        #IMU_idx = -1;
+        #while (time_ref > Ref_ts[IMU_idx+1] and IMU_idx < num_ts):
+        #    IMU_idx += 1;
     
-    #d_pos = (enu[IMU_idx+1]-enu[IMU_idx])/time_step*dt + enu[IMU_idx];             
-    #q_int = q[IMU_idx].slerp(q[IMU_idx],q[IMU_idx+1], dt/time_step);                   
+        
+        #difference from one step, but I;ll use another ?????
+        #dt = (time_ref - Ref_ts[IMU_idx]).double();
+    
+            
+        #time_step = (Ref_ts[IMU_idx+1] - Ref_ts[IMU_idx]).double();
+        #print (IMU_idx,dt,time_step)        
+        
+        #d_pos = (enu[IMU_idx+1]-enu[IMU_idx])/time_step*dt + enu[IMU_idx];             
+        #q_int = q[IMU_idx].slerp(q[IMU_idx],q[IMU_idx+1], dt/time_step);                   
 
     
         
-    return np.array(out),np.array(enus), np.array(frames)-1, np.array(qs)
+    return cnt#np.array(out),np.array(enus), np.array(frames)-1, np.array(qs)
     #d_pos, q_int
     
